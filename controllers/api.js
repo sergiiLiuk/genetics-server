8809a8769cde75f2e8ddd5889101f57703215fb8
Sergii Liuk <sergiiliuk@yahoo.dk>
2021-03-28 16:25:36 +0200

    test commit

--- controllers/api.js
+++ controllers/api.js
@@ -1,13 +1,13 @@
-// const BirthRecord = require("../models/birth")
+const BirthRecord = require("../models/birth")
  
-// module.exports = class API {
-//     static async fetchAllBirthRecords(req, res) {
-//         try {
-//             console.log('hello')
-//             const birthRecords = await BirthRecord
-//             res.status(200).json(birthRecords);
-//         } catch (err) {
-//             res.status(400).json({message: err.message});
-//         }
-//     }
-// }
+module.exports = class API {
+    static async fetchAllBirthRecords(req, res) {
+        try {
+            console.log('hello')
+            const birthRecords = await BirthRecord
+            res.status(200).json(birthRecords);
+        } catch (err) {
+            res.status(400).json({message: err.message});
+        }
+    }
+}
