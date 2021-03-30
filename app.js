4f4c1a495ab755e6c71be1a575acaf5b414eb500
Sergii Liuk <sergiiliuk@yahoo.dk>
2021-03-30 18:52:04 +0200

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const birthRecordSchema = require('./graphql/index').birthRecordSchema;
 const port = process.env.PORT // 5000
 
 app.use('*',cors())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());  
  
 app.listen(port, async () => {
